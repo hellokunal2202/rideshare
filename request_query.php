3f0f3973fde7b55ff86d8296fd65a7a0368f2425
Kunal Mali <malikunal2202@gmail.com>
2023-09-18 00:02:58 +0530

    added the funcntion of sending ride request to driver

--- request_query.php
+++ request_query.php
@@ -5,9 +5,11 @@
     if($_REQUEST['action']==='sendReq')
     {
         $driver_id  = $_REQUEST['id'];
+        $trip_id=$_REQUEST['trip_id'];
         $user_id = $_SESSION['user_id'];
+
         // $dateAdded_now = date('Y-m-d');
-        $request = "INSERT INTO riderequest(user_id, driver_id, status) VALUES ('$user_id','$driver_id','pending')";
+        $request = "INSERT INTO riderequest(user_id, driver_id, status,	trip_id) VALUES ('$user_id','$driver_id','pending','$trip_id')";
         $request_query = mysqli_query($link, $request);
 
         // $sql = "INSERT INTO requests (sendingfrom, sendingto, dateAdded) VALUES ('$reqSendingFrom', '$reqSendingTo', '$dateAdded_now')"; 
@@ -42,6 +44,22 @@
 
     }
     else if($_REQUEST['action']==='RequestSection'){
+        $request_id  = $_REQUEST['request_id'];
+        $user_id = $_SESSION['user_id'];
+        // $dateAdded_now = date('Y-m-d');
+        $update_req = "UPDATE riderequest SET status='accepted' WHERE request_id='$request_id'";
+        $result = mysqli_query($link, $update_req);
+        if($result){
+            $seat_query = "update carsharetrips set seatsavailable = seatsavailable-1 where trip_id = (select trip_id from riderequest where request_id = '$request_id')";
+            $seat_result = mysqli_query($link, $seat_query);
+            echo "Request Accepted";
+
+        }
+        else{
+            echo "Error";
+        }
+
+        
         
         
     }
